1fc51a32b6a1a4020bfc178414ea5ed393bd0670
unknown <matheusscosta0806@gmail.com>
2026-03-25 07:52:13 -0300

    ㅤ

---
+++ 3Dplot/3Dplot.py
@@ -0,0 +1,14 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+
+x3d = np.linspace(-5, 5, 100)
+y3d = np.linspace(-5, 5, 100)
+
+X3D, Y3D = np.meshgrid(x3d, y3d)
+Z3D = np.sin(np.sqrt(X3D**2 + Y3D**2))
+
+ax.plot_surface(X3D, Y3D, Z3D, cmap='viridis')
+plt.show()
